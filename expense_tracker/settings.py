b700c94a0a0623e8d0a56582728a6e57838c5f2f
DEBESH NAYAK <debeshnayak466@gmail.com>
2026-03-03 15:37:45 +0530

    Set DEBUG to True for development

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kldne)xey)1zn)pkjqjmin377#k&4ziy+d8_7c^1u@&zf_)1^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
